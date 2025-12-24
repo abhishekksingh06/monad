66400dff644ca81de2fe166b8ffab24a8c54c192
Abhishek Kumar Singh <abhishekksingh.dev@gmail.com>
2025-12-24 16:15:29 +0530

    feat(span): add unified Span type with ariadne and chumsky support
    - Introduce Span struct with source id and byte range
    - Implement ariadne::Span for diagnostic reporting
    - Implement chumsky::span::Span for parser integration
    - Add helper methods (len, is_empty)
    - Define Spanned<T> alias for convenience

--- crates/syntax/src/span.rs
+++ crates/syntax/src/span.rs
@@ -1,109 +1,69 @@
 use std::ops::Range;
 
-use miette::SourceSpan;
+pub type SourceId = usize;
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+#[derive(Debug, Default, Clone, PartialEq, Eq)]
 pub struct Span {
-    pub start: u32,
-    pub end: u32,
+    src: SourceId,
+    pub range: Range<usize>,
 }
 
 impl Span {
-    #[inline]
-    pub fn new(start: u32, end: u32) -> Self {
-        debug_assert!(start <= end);
-        Self { start, end }
-    }
-
-    #[inline]
-    pub fn len(self) -> u32 {
-        self.end - self.start
-    }
-
-    #[inline]
-    pub fn is_empty(self) -> bool {
-        self.start == self.end
-    }
-
-    #[inline]
-    pub fn join(self, other: Span) -> Span {
-        Span {
-            start: self.start.min(other.start),
-            end: self.end.max(other.end),
-        }
-    }
-}
-
-impl From<Range<u32>> for Span {
-    fn from(range: Range<u32>) -> Self {
-        Span::new(range.start, range.end)
-    }
-}
-
-impl From<Span> for Range<u32> {
-    fn from(span: Span) -> Self {
-        span.start..span.end
-    }
-}
-
-impl From<Span> for SourceSpan {
-    fn from(span: Span) -> Self {
-        (span.start as usize, span.len() as usize).into()
-    }
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-pub struct Spanned<T> {
-    pub inner: T,
-    pub span: Span,
-}
-
-impl<T> Spanned<T> {
-    #[inline]
-    pub fn new(inner: T, span: Span) -> Self {
-        Self { inner, span }
-    }
-
-    #[inline]
-    pub fn map<U, F>(self, f: F) -> Spanned<U>
-    where
-        F: FnOnce(T) -> U,
-    {
-        Spanned {
-            inner: f(self.inner),
-            span: self.span,
+    pub fn new(src: SourceId, start: usize, end: usize) -> Self {
+        Self {
+            src,
+            range: start..end,
         }
     }
 
-    #[inline]
-    pub fn as_ref(&self) -> Spanned<&T> {
-        Spanned {
-            inner: &self.inner,
-            span: self.span,
+    pub fn len(&self) -> usize {
+        self.range.end - self.range.start
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.range.start == self.range.end
+    }
+}
+
+impl ariadne::Span for Span {
+    type SourceId = SourceId;
+
+    fn source(&self) -> &Self::SourceId {
+        &self.src
+    }
+
+    fn start(&self) -> usize {
+        self.range.start
+    }
+
+    fn end(&self) -> usize {
+        self.range.end
+    }
+}
+
+impl chumsky::span::Span for Span {
+    type Context = SourceId;
+
+    type Offset = usize;
+
+    fn new(context: Self::Context, range: Range<Self::Offset>) -> Self {
+        Self {
+            src: context,
+            range,
         }
     }
 
-    #[inline]
-    pub fn as_mut(&mut self) -> Spanned<&mut T> {
-        Spanned {
-            inner: &mut self.inner,
-            span: self.span,
-        }
+    fn context(&self) -> Self::Context {
+        self.src
     }
-    #[inline]
-    pub fn with_span(self, span: Span) -> Self {
-        Spanned { span, ..self }
+
+    fn start(&self) -> Self::Offset {
+        self.range.start
+    }
+
+    fn end(&self) -> Self::Offset {
+        self.range.end
     }
 }
 
-impl<T> From<(T, Span)> for Spanned<T> {
-    fn from((inner, span): (T, Span)) -> Self {
-        Spanned::new(inner, span)
-    }
-}
-
-impl<T> From<(T, Range<u32>)> for Spanned<T> {
-    fn from((inner, range): (T, Range<u32>)) -> Self {
-        Spanned::new(inner, range.into())
-    }
-}
+pub type Spanned<T> = (T, Span);
